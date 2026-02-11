f05e033e2755eae9502d417ad7f4ffdc2d5bf656
satzznotyou <minakazehe@gmail.com>
2026-02-11 21:49:43 +0700

    Create Loader.ps1

---
+++ Loader.ps1
@@ -0,0 +1,20 @@
+Write-Host ""
+Write-Host "ALL IN OPTIMIZER PRO LOADER"
+Write-Host "Downloading latest version..."
+Write-Host ""
+
+$scriptUrl = "https://raw.githubusercontent.com/satzznotyou/optimizer/main/Optimizer.ps1"
+
+try {
+    $script = Invoke-RestMethod $scriptUrl
+    if ([string]::IsNullOrWhiteSpace($script)) {
+        Write-Host "Download failed or file empty."
+        pause
+        exit
+    }
+    Invoke-Expression $script
+}
+catch {
+    Write-Host "Failed to download script."
+    pause
+}
